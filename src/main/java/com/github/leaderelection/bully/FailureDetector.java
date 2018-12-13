8a7ac84f57f1543aff714697713940be8de81bcd
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2018-12-13 14:45:16 -0800

    Init iter of failure detector and member (skeletons). WIP - lots more work to do.

---
+++ src/main/java/com/github/leaderelection/bully/FailureDetector.java
@@ -0,0 +1,10 @@
+package com.github.leaderelection.bully;
+
+/**
+ * Skeleton for a failure detector.
+ * 
+ * @author gaurav
+ */
+public class FailureDetector {
+
+}
---
+++ src/main/java/com/github/leaderelection/bully/Member.java
@@ -0,0 +1,10 @@
+package com.github.leaderelection.bully;
+
+/**
+ * Skeleton for a member server participating in leader election as part of a group of servers.
+ * 
+ * @author gaurav
+ */
+public class Member {
+
+}
