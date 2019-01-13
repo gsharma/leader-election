fc339092e82cc38afbcf6b98ff060de815a052b4
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2019-01-13 15:20:44 -0800

    Handler for the election message.

--- src/main/java/com/github/leaderelection/MemberServiceHandler.java
+++ src/main/java/com/github/leaderelection/MemberServiceHandler.java
@@ -3,6 +3,7 @@ package com.github.leaderelection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.github.leaderelection.messages.ElectionRequest;
 import com.github.leaderelection.messages.OkResponse;
 import com.github.leaderelection.messages.Request;
 import com.github.leaderelection.messages.RequestType;
@@ -77,10 +78,37 @@ class MemberServiceHandler implements ServiceHandler {
           logger.info("Received::{}, Responded with::{}", request, response);
           break;
         case FD_FAILED:
+          // TODO
           break;
         case ELECTION:
-          // TODO: forward to relevant members
-          response = new OkResponse(request.getSenderId(), request.getEpoch());
+          final ElectionRequest electionRequest = ElectionRequest.class.cast(request);
+          final Id senderId = electionRequest.getSenderId();
+          int comparisonResult = senderId.compareTo(sourceMember.getId());
+          switch (comparisonResult) {
+            case 1:
+              response = new OkResponse(request.getSenderId(), request.getEpoch());
+              break;
+            case 0:
+              // do nothing
+              break;
+            case -1:
+              // If P receives an Election message from another process with a lower ID it sends an
+              // Answer message back and starts the election process at the beginning, by sending an
+              // Election message to higher-numbered processes.
+              response = new OkResponse(request.getSenderId(), request.getEpoch());
+              // forward to relevant members
+              for (final Member largerMember : memberGroup.largerMembers(sourceMember)) {
+                final ElectionRequest election =
+                    new ElectionRequest(sourceMember.getId(), sourceMember.currentEpoch());
+                try {
+                  final byte[] responsePayload = transport.send(largerMember.getHost(),
+                      largerMember.getPort(), InternalLib.serialize(election));
+                } catch (Exception problem) {
+                  logger.error("Failed in election-request probe to " + largerMember, problem);
+                }
+              }
+              break;
+          }
           logger.info("Received::{}, Responded with::{}", request, response);
           break;
         case COORDINATOR:
